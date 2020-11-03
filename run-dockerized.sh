38d0eaba1f4cbe1bcf88aab4838d8718d035c7d0
cyfrost <cyrus.frost@hotmail.com>
2020-11-03 20:42:37 +0530

    also stop and remove old continer instance

--- run-dockerized.sh
+++ run-dockerized.sh
@@ -5,5 +5,7 @@ container_tag="url-expander"
 
 rm -rf "$build_out"
 go build -ldflags "-linkmode external -extldflags -static" -o "$build_out" "$prg" || exit 1
+docker stop $container_tag
+docker rm $container_tag
 docker build --no-cache -t "$container_tag:1.0" .
 docker run --publish $port:$port --name $container_tag $container_tag:1.0
